6830be7fa40dd708b3849ec597dcc304c5f31f0d
Darren Minsoo Kim <darren_minsoo_kim@outlook.com>
2020-08-27 19:27:53 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Akida {
+  class Program {
+      static void Main(string[] args)
+      {
+        
+        // Connection information
+        const string con_info = 
+            "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=TDB.accdb";
+
+        // Insertion
+        void insertRecord() {   
+          using (var con = new OleDbConnection(con_info)) {
+            var sql = 
+              "INSERT INTO Person " + 
+              "(FirstName, LastName, Email) " + 
+              "Values ('ABC', 'DEF', 'RTR@CFC.COM')";
+            var cmd = new OleDbCommand(sql, con);
+              try {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Inserted!");}
+              catch (Exception ex) {
+                Console.WriteLine(ex.Message);}}};
+
+          // Read
+          void readRecord() {   
+            using (var con = new OleDbConnection(con_info)) {
+              var sql = "SELECT * FROM Person";
+              var cmd = new OleDbCommand(sql, con);
+              try {
+                con.Open();
+                using (var reader = cmd.ExecuteReader()) {
+                  while (reader.Read()) {
+                    Console.WriteLine("{0}", reader["FirstName"].ToString());}}}
+              catch (Exception ex) {
+                Console.WriteLine(ex.Message);}}};
+
+          // execution
+          insertRecord();
+          readRecord();
+      }}}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my C# code to test connection to MS-Access database file.
